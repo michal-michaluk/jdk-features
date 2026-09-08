8a9af2158e9087d2bcffb47fefcf257dbe475129
Michał Michaluk <michaluk.michal@gmail.com>
2026-09-08 15:28:30 +0200

    starter-04: demos + step03 full solution (base) + step04 README (task)

--- src/main/java/dev/bottega/jdkfeatures/flightcontrol/step06/domain/Circle.java
+++
@@ -1,6 +0,0 @@
-package dev.bottega.jdkfeatures.flightcontrol.step06.domain;
-
-import java.util.Map;
-
-public record Circle(Point center, double radius, String label, Map<String, String> props) implements Area {
-}
