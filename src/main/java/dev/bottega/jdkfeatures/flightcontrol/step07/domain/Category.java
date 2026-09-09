35816e864461c4c4d615bd585f948c05d4bf345d
Michał Michaluk <michaluk.michal@gmail.com>
2026-09-09 08:10:06 +0200

    starter-06: demos + step05 full solution (base) + step06 README (task)

--- src/main/java/dev/bottega/jdkfeatures/flightcontrol/step07/domain/Category.java
+++
@@ -1,6 +0,0 @@
-package dev.bottega.jdkfeatures.flightcontrol.step07.domain;
-
-/** Threat classification of an aircraft relative to a sector and alarm speed. */
-public enum Category {
-    ALARM, SECTOR, NORMAL, UNKNOWN
-}
